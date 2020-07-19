2a0b21d55ad5be4e79c5770602f4492d8f90d9f7
Carlos Calvo <carloscalvonazabal@gmail.com>
2020-07-19 15:44:19 -0300

    Recuperatorio TP1

---
+++ Entidades/Numero.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        #region Atributos
+
+        private double numero;
+
+        #endregion
+
+        #region Propiedades
+        private string SetNumero
+        {
+            set 
+            { 
+                this.numero = this.ValidarNumero(value); 
+            }
+        }
+        
+        #endregion
+        
+        #region Métodos
+
+
+        /// <summary>
+        /// Convierte un string que representa un número binario en un string que representa su notación decimal.
+        /// </summary>
+        /// <param name="binario"> string: Número binario a ser convertido </param>
+        /// <returns> string: Notación decimal del número pasado como parámetro </returns>
+        public string BinarioDecimal(string binario)
+        {
+            string rta = "Valor invalido.";
+            int num;
+            bool isNumeric = EsBinario(binario);
+
+            if (isNumeric == true)
+            {
+                num = Convert.ToInt32(binario, 2);
+
+                if (num >= 0)
+                {
+                    rta = num.ToString();
+                }
+                else
+                {
+                    num = Math.Abs(num);
+                    rta = num.ToString();
+                }
+            }
+
+            return rta;
+        }
+
+        /// <summary>
+        /// Convierte un string que representa un número decimal en un string que representa el mismo número en notación binaria.
+        /// </summary>
+        /// <param name="numero"> string: número decimal a ser convertido en binario </param>
+        /// <returns> string: notación binaria del número decimal pasado como parámetro </returns>
+        public string DecimalBinario(string numero)
+        {
+            string rta;
+            bool isNumeric = Int32.TryParse(numero, out int num);
+            if (isNumeric == true)
+            {
+                rta = DecimalBinario(num);
+            }
+            else
+            {
+                rta = "Valor inválido.";
+            }
+            return rta;
+        }
+
+        /// <summary>
+        /// Convierte un double en un string que representa el mismo número en notación binaria
+        /// </summary>
+        /// <param name="numero"> Double: número a ser convertido en binario </param>
+        /// <returns> string: notación binaria del número decimal pasado como parámetro </returns>
+        public string DecimalBinario(double numero)
+        {
+            string rta = "Valor Invalido";
+            int aux = (int)numero;
+            int bin;
+
+            if (aux < 0)
+            {
+                aux = Math.Abs(aux);
+            }
+
+            if (aux >= 0)
+            {
+                rta = "";
+                
+                if(aux == 0)
+                {
+                    rta = "0";
+                }
+                else
+                {
+                    while (aux >= 1)
+                    {
+                        bin = aux % 2;
+                        aux /= 2;
+                        rta = (bin.ToString() + rta);
+                    }
+                }                
+            }
+            return rta;
+        }
+
+        private bool EsBinario(string numero)
+        {
+            bool aux = true;
+
+            foreach(char c in numero)
+            {
+                if(!(c == '1' || c == '0'))
+                {
+                    aux = false;
+                    break;
+                }
+            }
+            return aux;
+        }
+
+        #endregion
+
+        #region Constructores
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+        }
+
+        #endregion
+
+        #region Sobrecarga de Operadores
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1.numero / n2.numero;
+        }
+
+        #endregion
+
+        #region Validación
+
+        /// <summary>
+        /// Permite validar si un string ingresado como parámetro es un número
+        /// </summary>
+        /// <param name="strNumero"> string: la cadena a ser evaluada </param>
+        /// <returns> double: si la cadena era un numero, lo devuelve como double; si no, devuelve 0 </returns>
+        private double ValidarNumero(string strNumero)
+        {
+            double.TryParse(strNumero, out double num);
+            return num;
+        }
+
+        #endregion
+
+    }
+}
---
+++ MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+
+        #region Constructor
+
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Eventos de Botones
+
+        private void BtnOperar_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text).ToString();
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Numero auxiliar = new Numero();
+            this.lblResultado.Text = auxiliar.DecimalBinario(this.lblResultado.Text);
+        }
+
+        private void BtnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero auxiliar = new Numero();
+            this.lblResultado.Text = auxiliar.BinarioDecimal(this.lblResultado.Text);
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.cmbOperador.SelectedItem = "+";
+            this.lblResultado.Text = "";
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            double rta;
+            Numero aux1 = new Numero(numero1);
+            Numero aux2 = new Numero(numero2);
+
+            rta = Calculadora.Operar(aux1, aux2, operador);
+
+            return rta;
+        }
+
+        #endregion
+
+    }
+}
